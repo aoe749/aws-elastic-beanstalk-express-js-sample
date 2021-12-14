dddee422f0bcf71fa644d4a9f1bc9babad79d49e
aoe749 <viktormuradyan@gmail.com>
2021-12-14 11:06:23 +0400

    changed message2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Armenia!'));
+app.get('/', (req, res) => res.send('Bari halust YYerevan!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
